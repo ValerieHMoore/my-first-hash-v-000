95e7c2bdd3249dea2b22ffca9bff0bf09086f86c
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 16:00:14 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-singers = {"Bass-Baritone" => "Bryn", "Bass" => "Ildar"}
+  singers = {"Bass-Baritone" => "Bryn", "Bass" => "Ildar"}
 end
 
 
